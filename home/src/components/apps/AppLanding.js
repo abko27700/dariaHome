f23a7ae0d2e80b7e534f7c880262d548cc0edd37
abko27700 <abko27700@gmail.com>
2024-06-05 16:28:15 -0400

    added react logic

--- home/src/components/apps/AppLanding.js
+++ home/src/components/apps/AppLanding.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './styling/AppLanding.css';
 
 export default function AppLanding() {
